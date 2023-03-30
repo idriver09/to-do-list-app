d695dd321ebb0c48c25381f86906aa7b9831bdc4
idriver09 <126793168+idriver09@users.noreply.github.com>
2023-03-30 15:55:04 -0400

    test

--- script.js
+++ script.js
@@ -4,6 +4,5 @@ let container = $('.container');
 let userInput = $('#input').val();
 let list = $('#list');
 let li = (<li></li>);
- $('#button').addClass('test');
 list.append(li);
 li.append(userInput);
