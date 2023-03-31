fca002507bff42a4b384477af21f8e6fbe039d3b
idriver09 <126793168+idriver09@users.noreply.github.com>
2023-03-30 23:58:18 -0400

    added functionality to to-do-list

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-let container = $('.container');
-
-
+function newItem(){
 let userInput = $('#input').val();
 let list = $('#list');
-let li = (<li></li>);
+let li = $('<li></li>');
+let crossOutButton = $('<button></button)');
+crossOutButton.append(document.createTextNode('X'));
+crossOutButton.addClass("crossOutButton");
+
 list.append(li);
 li.append(userInput);
+li.append(crossOutButton);
+
+
+
+if (userInput === ''){
+    alert('input required');
+}
+else {
+    list.append.li;
+}
+
+li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+ });
+
+ crossOutButton.on("click", function (){
+        li.addClass("delete");
+ });
+
+$('#list').sortable();
+
+}
